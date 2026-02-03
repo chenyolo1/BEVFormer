557b6cb5e4c8784b3f8c64e964ea4622df72eeec
developer <developer@example.com>
2026-02-03 17:22:53 +0800

    typo fix

--- projects/configs/bevformer/bevformer_tiny.py
+++ projects/configs/bevformer/bevformer_tiny.py
@@ -23,7 +23,7 @@ voxel_size = [0.2, 0.2, 8]
 
 
 
-img_norm_cfg = dict(␊
+img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=False)
 
 # For nuScenes we usually do 10-class detection
